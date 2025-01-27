f5452cfb0d0074ba0c1e0e8c15548441ecf924bb
soumyatata <soumyatata5a1@gmail.com>
2025-01-27 13:37:07 +0000

    update the app code

---
+++ src/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+# Create a Flask instance
+app = Flask(__name__)
+
+# Define a route for the root URL
+@app.route('/')
+def hello_world():
+    return '''
+    <html>
+        <head><title>Welcome Page</title></head>
+        <body>
+            <h1>Welcome to My Page</h1>
+            <p>This is my FLASK WEB APP to deploy in AWS through GitHub Actions pipeline</p>
+        </body>
+    </html>
+    '''
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
